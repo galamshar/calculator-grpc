e1d0ded52af74c72a20eeba178e285dda9d496c1
galamshar <myrzagalyev.galymzhan@gmail.com>
2021-02-26 10:58:27 +0600

    Final result

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/galamshar/calculator-grpc/calculator/calculator_pb"
 	"google.golang.org/grpc"
 	"log"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Enter the num(s) : ")
+	var nums []int32
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("One number for get prime number decomposition\nMore than one number for get average\nEnter the num(s) : ")
+
+	numbers, _ := reader.ReadString('\n')
+
+	regex := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+	allString := regex.FindAllString(numbers, -1)
+
+	for _, element := range allString {
+		temp, err := strconv.ParseInt(element, 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums = append(nums, int32(temp))
+	}
 
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -19,13 +38,17 @@ func main() {
 
 	c := calculator_pb.NewCalculatorServiceClient(conn)
 
-	PrimeNumberDecomposition(c)
+	if len(nums) == 1 {
+		PrimeNumberDecomposition(c, nums[0])
+	} else {
+		ComputeAverage(c, nums)
+	}
 }
 
-func PrimeNumberDecomposition(c calculator_pb.CalculatorServiceClient) {
+func PrimeNumberDecomposition(c calculator_pb.CalculatorServiceClient, num int32) {
 	ctx := context.Background()
 	req := &calculator_pb.PrimeNumberDecompositionRequest{
-		Number: 120,
+		Number: num,
 	}
 
 	response, err := c.PrimeNumberDecomposition(ctx, req)
@@ -34,9 +57,20 @@ func PrimeNumberDecomposition(c calculator_pb.CalculatorServiceClient) {
 		log.Fatalf("error while calling PrimeNumberDecomposition RPC %v", err)
 	}
 
-	log.Println(response)
+	log.Println(response.Answer)
 }
 
-func ComputeAverage(c calculator_pb.CalculatorServiceClient) {
+func ComputeAverage(c calculator_pb.CalculatorServiceClient, nums []int32) {
+	ctx := context.Background()
+	req := &calculator_pb.ComputeAverageRequest{
+		Numbers: nums,
+	}
 
+	response, err := c.ComputeAverage(ctx, req)
+
+	if err != nil {
+		log.Fatalf("error while calling ComputeAverage RPC %v", err)
+	}
+
+	log.Println(response.Answer)
 }
